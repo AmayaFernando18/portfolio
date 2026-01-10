c142ab553480d8a5ae25ee6681e464727a013070
Amaya Fernando <fernandowdna.22@uom.lk>
2026-01-10 08:19:23 +0530

    Added new cv

--- .history/app/components/Header_20251009173521.jsx
+++ .history/app/components/Header_20251009173521.jsx
@@ -52,9 +52,9 @@ const Header = () => {
                 initial={{y: 30, opacity: 0}}
               whileInView={{y: 0, opacity: 1}}
               transition={{duration: 0.6, delay: 1}}
-                href='/AmayaFernando_SE_CV.pdf' download 
+                href='/amaya-resume.pdf' download 
                 className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>
-                    My Resume <Image src={assets.download_icon} alt='whitearrow' className='w-4' /></motion.a>
+                    my resume <Image src={assets.download_icon} alt='whitearrow' className='w-4' /></motion.a>
             </div>
     </div>
   )
